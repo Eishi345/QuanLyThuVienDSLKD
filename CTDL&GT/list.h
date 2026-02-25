aee2084dc71194cd0484f09c608939e9a7a1add1
Eishi345 <agakicuksuk345@gmail.com>
2026-02-25 11:04:16 +0700

    Update list.h

--- CTDL&GT/list.h
+++ CTDL&GT/list.h
@@ -4,8 +4,8 @@
 #include "struct.h"
 
 SNode* createSNode(Reader r);	//Hàm tạo node mới cho danh sách liên kết
-void initList(SNode** head);	//Khởi tạo danh sách liên kết rỗng
-void addReader(SNode** head, Reader r);	//Thêm một bạn đọc vào cuối danh sách
+void initSList(SNode** head);	//Khởi tạo danh sách liên kết rỗng
+void insertTail(SNode** head, Reader r);	//Thêm một bạn đọc vào cuối danh sách
 void displayReaders(SNode* head);	//Hiển thị danh sách bạn đọc
 void freeList(SNode** head);	//Giải phóng toàn bộ bộ nhớ của danh sách liên kết
 
